f660d75d3713fb6fecab2e6eeef57dc327a531d2
Crhistian26 <jhan.cterreroramirez26@gmail.com>
2025-05-25 16:42:25 -0400

    Creation of part 1 and 2 the task 1, and edition the document with the questionnaire

--- src/HW1-Questionnaire/HW1-Program/HW1-ConsoleApp/Program.cs
+++ src/HW1-Questionnaire/HW1-Program/HW1-ConsoleApp/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
+            #region Part 1, Declare variables
+
+            bool decision = true;
+            char oneCharacter = 'a';
+            string chain = "hello i am string";
+            int number = int.MaxValue;
+            long longNumber = long.MaxValue;
+            float floatNumber = float.MaxValue;
+            double doubleNumber = double.MaxValue;
+            decimal decimalNumber = decimal.MaxValue;
+
+            Console.WriteLine(decision);
+            Console.WriteLine(oneCharacter);
+            Console.WriteLine(chain);
+            Console.WriteLine(number);
+            Console.WriteLine(longNumber);
+            Console.WriteLine(floatNumber);
+            Console.WriteLine(doubleNumber);
+            Console.WriteLine(decimalNumber);
+
+            #endregion
+
+            #region Part 2, Const in C#
+            const string nameProgram = "Homework console program";
             
+            Console.WriteLine(nameProgram);
+
+            //nameProgram = "Hello";
+            #endregion
         }
     }
 }
