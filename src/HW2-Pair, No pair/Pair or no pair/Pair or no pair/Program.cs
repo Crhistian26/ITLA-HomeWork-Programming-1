13e0b9645f02b9d2298b310000361e4c23332247
Crhistian26 <jhan.cterreroramirez26@gmail.com>
2025-06-01 22:26:24 -0400

    Creation for V1 program

--- src/HW2-Pair, No pair/Pair or no pair/Pair or no pair/Program.cs
+++ src/HW2-Pair, No pair/Pair or no pair/Pair or no pair/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Ingresa un numero");
+      
+            try
+            {
+                int number = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(number);
+                if(number %  2 == 0)
+                {
+                    Console.WriteLine("Tu numero es par");
+                }
+                else
+                {
+                    Console.WriteLine("Tu numero no es par");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("No ingresaste un numero");
+            }
         }
     }
 }
