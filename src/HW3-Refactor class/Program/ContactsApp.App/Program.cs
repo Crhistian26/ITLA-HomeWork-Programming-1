5fc24125791333c57e4e2ae5ae671cfb96129e77
Crhistian26 <jhan.cterreroramirez26@gmail.com>
2025-07-12 16:30:36 -0400

    Creation Method PresentationApp

--- src/HW3-Refactor class/Program/ContactsApp.App/Program.cs
+++ src/HW3-Refactor class/Program/ContactsApp.App/Program.cs
@@ -13,7 +13,51 @@ namespace ContactsApp.App
             new Contact(1,"Alberto","Gutierrez","8096785678","Los Alcarrizos","Alberto@gmail.com",21,true),
             new Contact(2,"Henrique","Carrasco","8490987685" ,"Federico Guzman","Carrasco@gmail.com",23,false)
         };
-    
+
+        #region Metodos para obtener datos
+        static int PresentationApp()
+        {
+            int choice = 0; bool i = false;
+
+            do
+            {
+                Console.WriteLine("Bienvenido a mi lista de Contactos\n");
+                Console.WriteLine("1)Ver contactos.  2)Detalles de un contacto.  3)Agregar contacto.  4)Modificar un contacto.  5)Eliminar un contacto.");
+                Console.WriteLine("0)Salir de la app\n");
+                Console.Write("Elija entre las opciones disponibles: ");
+                var f = Console.ReadLine();
+
+
+                if (!int.TryParse(f, out choice))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\n\nDebe ser un valor numerico.\nPresiona una tecla para continuar...");
+                    Console.ReadKey();
+                    Console.Clear();
+                    i = true;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    continue;
+                }
+
+                if (choice > 5 || choice < 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\n\nDebe ser un valor numerico entre el rango de 0 a 5, lea las opciones.\nPresiona una tecla para continuar...");
+                    Console.ReadKey();
+                    Console.Clear();
+                    i = true;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    continue;
+                }
+
+                choice = Convert.ToInt32(f);
+                i = false;
+
+            } while (i);
+
+            return choice;
+        }
+        #endregion
         static void Main(string[] args)
         {
         }
