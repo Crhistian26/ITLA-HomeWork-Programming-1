6001a9f2e1752187c11d1f4ea28580d710dd071b
Crhistian26 <jhan.cterreroramirez26@gmail.com>
2025-07-12 16:29:11 -0400

    Creation List of Contact

--- src/HW3-Refactor class/Program/ContactsApp.App/Program.cs
+++ src/HW3-Refactor class/Program/ContactsApp.App/Program.cs
@@ -8,6 +8,14 @@ namespace ContactsApp.App
 {
     internal class Program
     {
-
+        static List<Contact> contacts = new List<Contact>()
+        {
+            new Contact(1,"Alberto","Gutierrez","8096785678","Los Alcarrizos","Alberto@gmail.com",21,true),
+            new Contact(2,"Henrique","Carrasco","8490987685" ,"Federico Guzman","Carrasco@gmail.com",23,false)
+        };
+    
+        static void Main(string[] args)
+        {
+        }
     }
 }
