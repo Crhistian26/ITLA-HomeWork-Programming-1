5903add38cc9c943e9293d73d481f7c911c1182d
Crhistian26 <jhan.cterreroramirez26@gmail.com>
2025-07-12 17:09:11 -0400

    Change name for the console proyect

--- src/HW4-Contacts with POO/ContactsApp/ContactsApp.Console/Program.cs
+++ src/HW4-Contacts with POO/ContactsApp/ContactsApp.Console/Program.cs
@@ -1,10 +1,10 @@
-﻿namespace ContactsApp.Console
+﻿namespace ContactsApp.Visual
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
