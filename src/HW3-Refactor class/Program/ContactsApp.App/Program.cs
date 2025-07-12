0cca65c8303b565d923e8826cca60b5f95756c85
Crhistian26 <jhan.cterreroramirez26@gmail.com>
2025-07-12 16:43:14 -0400

    Creating the loop for the application

--- src/HW3-Refactor class/Program/ContactsApp.App/Program.cs
+++ src/HW3-Refactor class/Program/ContactsApp.App/Program.cs
@@ -224,6 +224,36 @@ namespace ContactsApp.App
         #endregion
         static void Main(string[] args)
         {
+            bool resetMain = false;
+            do
+            {
+                Console.Clear();
+                int choicePresentation = PresentationApp();
+
+                switch (choicePresentation)
+                {
+                    case 1:
+
+                        break;
+
+                    case 2:
+
+                        break;
+
+                    case 3:
+
+                        break;
+                    case 4:
+                        
+                        break;
+                    case 5:
+                       
+                        break;
+                    case 0:
+                       
+                        break;
+                }
+            } while (resetMain);
         }
     }
 }
